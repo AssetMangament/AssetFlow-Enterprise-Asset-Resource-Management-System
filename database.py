7a2ed2c7099bd8f13a2d9b2fbcdaea98ee1900d1
Saswat Palo <palosaswat@gmail.com>
2026-07-12 17:23:18 +0530

    Change database URL to PostgreSQL
    Updated database connection URL to use PostgreSQL.

--- database.py
+++ database.py
@@ -2,13 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./assetflow.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres.fsoyrtedsnprzkkpmqmn:SaswatAnanyaSwayam@aws-0-ap-northeast-1.pooler.supabase.com:5432/postgres"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
 def get_db():
     db = SessionLocal()
     try:
